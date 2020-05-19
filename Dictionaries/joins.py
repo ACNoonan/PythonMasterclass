84bad073b8e5c9e0193b9caa7e54445072d6db44
Adam Noonan <adamcnoonan@gmail.com>
2020-05-19 19:20:10 -0400

    small update

---
+++ Dictionaries/joins.py
@@ -0,0 +1,10 @@
+my_list = ['a', 'b', 'c', 'd']
+letters = 'abcdefghijklmnopqrstuvwxyz'
+numbers = '123456789'
+
+new_string = ''
+
+
+new_string =' mississippi, '.join(numbers)
+
+print(new_string)
---
+++ Dictionaries/locations.py
@@ -0,0 +1,6 @@
+locations = {0: 'You are at home learning Python.',
+             1: 'You are standing at the end of the road before a small brick building',
+             2: '',
+             3: '',
+             4: '',
+             5: ''}
