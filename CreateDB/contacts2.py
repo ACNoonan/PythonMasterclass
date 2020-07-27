650e1c28492275e340f6224930c9a131d653a874
Adam Noonan <adamcnoonan@gmail.com>
2020-07-27 10:42:30 -0400

    7/27: sqlite3 work, SQL injections, commits, etc.

--- CreateDB/contacts2.py
+++ CreateDB/contacts2.py
@@ -2,7 +2,22 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 
-for row in db.execute("SELECT * FROM contacts"):
-    print(row)
+new_email = "bitchass@cuntsinc.com"
+phone = input("Give us a number then, love: ")
+
+update_sql = "UPDATE contacts SET email = ? WHERE phone = ?"
+update_cursor = db.cursor()
+update_cursor.executescript(update_sql)
+print("{} rows updated".format(update_cursor.rowcount))
+
+
+update_cursor.connection.commit()
+update_cursor.close()
+
+for name, phone, email in db.execute("SELECT * FROM contacts"):
+    print(name)
+    print(phone)
+    print(email)
+    print("-" * 20)
 
 db.close()
--- CreateDB/createDB.py
+++ CreateDB/createDB.py
@@ -2,18 +2,25 @@ import sqlite3
 
 db = sqlite3.connect("contacts.sqlite")
 db.execute("CREATE TABLE IF NOT EXISTS contacts (name TEXT, phone INTEGER, email TEXT)")
-db.execute("INSERT INTO contacts(name, phone, email) VALUES('Adam', 9197457023, "           
+db.execute("INSERT INTO contacts(name, phone, email) VALUES('Adam', 9197457023, "
            "'an@aol.com')")
 db.execute("INSERT INTO contacts(name, phone, email) VALUES('Brian', 9197777023, "
            "'brian@aol.com')")
-
-
-
 cursor = db.cursor()
 cursor.execute("SELECT * FROM contacts")
+
+print(cursor.fetchone())
+print(cursor.fetchone())
+print(cursor.fetchone())
+
+
 for name, phone, email in cursor:
     print(name)
     print(phone)
     print(email)
     print('-' * 40)
+
+
+cursor.close()
+db.commit()
 db.close()
